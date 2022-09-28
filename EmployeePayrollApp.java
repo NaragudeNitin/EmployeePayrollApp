049a627c13d251fc9ffa5861c74f1ee296664034
NaragudeNitin <naragudenitin@gmail.com>
2022-09-28 23:26:26 +0530

    Adding code to part time and employee wage

--- EmployeePayrollApp.java
+++ EmployeePayrollApp.java
@@ -6,16 +6,24 @@ public class EmployeePayrollApp {
 
 	public static void main(String[] args) {
 		Random random = new Random();
-		int rand = random.nextInt(2);
+		int rand = random.nextInt(3);
 		System.out.println("Your Random num is " + rand);
 
 		int wagePerHour = 20;
-		int fulltime = 8;
+		int fulltime = 12;
+
+		// This is part time employee problem.
+		int partTime = 8;
 		if (rand == 1) {
 			System.out.println("You are present today so");
 			int dailyWage;
 			dailyWage = fulltime * wagePerHour;
 			System.out.println("Your today's salary is: " + dailyWage + "Rs");
+		} else if (rand == 2) {
+			System.out.println("You are part time employer");
+			int partTimeWage;
+			partTimeWage = partTime * wagePerHour;
+			System.out.println("Your part time salary is: " + partTimeWage + "Rs");
 		} else {
 			System.out.println("Employee is Absent");
 		}
