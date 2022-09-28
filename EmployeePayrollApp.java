79f8c50dc5d09ea9fc8feebe0ccd05ff8b5bce1b
NaragudeNitin <naragudenitin@gmail.com>
2022-09-28 19:18:52 +0530

    Added code to find Daily Wage of the employee

--- EmployeePayrollApp.java
+++ EmployeePayrollApp.java
@@ -9,11 +9,15 @@ public class EmployeePayrollApp {
 		int rand = random.nextInt(2);
 		System.out.println("Your Random num is " + rand);
 
-		if (rand == 0) {
-			System.out.println("Employee is Absent");
+		int wagePerHour = 20;
+		int fulltime = 8;
+		if (rand == 1) {
+			System.out.println("You are present today so");
+			int dailyWage;
+			dailyWage = fulltime * wagePerHour;
+			System.out.println("Your today's salary is: " + dailyWage + "Rs");
 		} else {
-			System.out.println("Employee is present");
+			System.out.println("Employee is Absent");
 		}
-
 	}
 }
