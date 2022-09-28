bb95ac97224d73ab7ad0b5afd63291b487cbf367
NaragudeNitin <naragudenitin@gmail.com>
2022-09-28 23:32:24 +0530

    Adding Employee daily pay roll using switch case

--- EmployeePayrollApp.java
+++ EmployeePayrollApp.java
@@ -3,29 +3,32 @@ package com.employee.wage;
 import java.util.Random;
 
 public class EmployeePayrollApp {
+	private static int WagePerHr=20;
 
 	public static void main(String[] args) {
 		Random random = new Random();
-		int rand = random.nextInt(3);
-		System.out.println("Your Random num is " + rand);
-
-		int wagePerHour = 20;
-		int fulltime = 12;
-
-		// This is part time employee problem.
-		int partTime = 8;
-		if (rand == 1) {
-			System.out.println("You are present today so");
-			int dailyWage;
-			dailyWage = fulltime * wagePerHour;
-			System.out.println("Your today's salary is: " + dailyWage + "Rs");
-		} else if (rand == 2) {
-			System.out.println("You are part time employer");
-			int partTimeWage;
-			partTimeWage = partTime * wagePerHour;
-			System.out.println("Your part time salary is: " + partTimeWage + "Rs");
-		} else {
-			System.out.println("Employee is Absent");
+		int rand =random.nextInt(3);
+		int fullSalary;
+		int partSalary;
+		int fulltime=8;
+		int parttime=4;
+		switch (rand) {
+		case 0:
+			System.out.println("you were absent today");
+			break;
+		case 1:
+			System.out.println("you are present today.");
+			fullSalary=WagePerHr*fulltime;
+			System.out.println("Your full time salary is "+ fullSalary+ "Rs");
+			break;
+		case 2:
+			System.out.println("You did Part time today.");
+			partSalary= WagePerHr*parttime;
+			System.out.println("Your part time salary is: "+partSalary + "Rs");
+			break;
+		default:
+			System.out.println("you are not employee of our company.");
+			break;
 		}
 	}
 }
