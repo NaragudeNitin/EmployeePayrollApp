fd6f0c8d1b736847b8f44a0c55e7b653bac15db4
NaragudeNitin <naragudenitin@gmail.com>
2022-09-28 19:06:18 +0530

    Added Welcome messege on master Branch

---
+++ EmployeePayrollApp.java
@@ -0,0 +1,9 @@
+package com.employee.wage;
+
+
+public class EmployeePayrollApp {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to employee pay roll application.");
+	}
+}
