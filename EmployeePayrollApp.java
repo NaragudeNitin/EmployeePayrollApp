7dc5e8020ae31eaaadac0317d9dab95ae11c8ac9
NaragudeNitin <naragudenitin@gmail.com>
2022-09-28 19:10:12 +0530

    Added code to find the employee attendence

--- EmployeePayrollApp.java
+++ EmployeePayrollApp.java
@@ -1,9 +1,19 @@
 package com.employee.wage;
 
+import java.util.Random;
 
 public class EmployeePayrollApp {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to employee pay roll application.");
+		Random random = new Random();
+		int rand = random.nextInt(2);
+		System.out.println("Your Random num is " + rand);
+
+		if (rand == 0) {
+			System.out.println("Employee is Absent");
+		} else {
+			System.out.println("Employee is present");
+		}
+
 	}
 }
