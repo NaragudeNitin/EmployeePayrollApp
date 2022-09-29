4b52b60755b0a38c2cab56a79c49689ffb1ae040
NaragudeNitin <naragudenitin@gmail.com>
2022-09-29 21:50:04 +0530

    Adding code to Calculate Wages till

--- EmployeePayrollApp.java
+++ EmployeePayrollApp.java
@@ -3,34 +3,45 @@ package com.employee.wage;
 import java.util.Random;
 
 public class EmployeePayrollApp {
-	private static final String FULL_TIME ="FULL_TIME";
-	private static final String PART_TIME ="PART_TIME";
-	private static final int monthDays =20;
-	
+	private static final String FULL_TIME = "FULL_TIME";
+	private static final String PART_TIME = "PART_TIME";
+	private static final int monthDays = 20;
+
 	public static void main(String[] args) {
-		EmployeePayrollApp full = new EmployeePayrollApp();
-		full.noOfHrsInDay(FULL_TIME);
-		
-		int FulltimeSalary= monthDays*(full.dailySal(20, 8));
-		System.out.println("Your full time monthly salary is "+FulltimeSalary);
-//		salary=no of days* Daily salary
-		int PartTimeSal = monthDays * (full.dailySal(20, 4));
-		System.out.println("Your Monthly Part time salary is "+ PartTimeSal);
-		
+		EmployeePayrollApp nitin = new EmployeePayrollApp();
+		int hrs = nitin.noOfHrsInDay(FULL_TIME);
+		System.out.println("working time: " + hrs);
+
+		double salary = nitin.oneDaySal();
+		System.out.println("one day salary is " + salary);
+		double fullMonthSal = salary * monthDays;
+		System.out.println("so your 20 days full time sal is: " + fullMonthSal);
+
+		double sal100hr = nitin.hundredHrsal(100, 20);
+		System.out.println("Your salary for 100 working hours is: " + sal100hr);
+
 	}
-	
+
 	public int noOfHrsInDay(String type) {
 		switch (type) {
 		case FULL_TIME:
 			return 8;
-		case PART_TIME :
+		case PART_TIME:
 			return 4;
 		default:
 			return 0;
 		}
 	}
-	
-	public int dailySal(int time, int hrwage) {
-		return time * hrwage;
+
+	public double oneDaySal() {
+		return noOfHrsInDay(FULL_TIME) * 20;
+	}
+
+	public double oneDaysal(int wrHr) {
+		return noOfHrsInDay(PART_TIME) * wrHr;
+	}
+
+	public double hundredHrsal(int val1, int val2) {
+		return val1 * val2;
 	}
 }
